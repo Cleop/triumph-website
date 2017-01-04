a9aa7bf07005ef7f97df576426120f61a4d0c7ee
Cleop <cleo.pearson@hotmail.co.uk>
2017-01-04 15:59:15 +0000

    Dots are now visible

---
+++ public/testimonial-carousel.js
@@ -0,0 +1,37 @@
+  <img class="mySlides" src="img_nature_wide.jpg" style="width:100%">
+  <div class="w3-center w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+</div>
+
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function showDivs(index) {
+  let i;
+  const testimonials = document.getElementsByClassName("testimonial");
+  const dots = document.getElementsByClassName("demo");
+  // If the current index surpasses the number of testimonials, reset it to the first slide
+  if (index > testimonials.length) {slideIndex = 1}
+  // Loop through the testimonials and set all of their displays to none
+  for (i = 0; i < testimonials.length; i++) {
+     testimonials[i].style.display = "none";
+  }
+  // Loop through all of the dots and set all of them to the empty dot class name, replacing the filled one
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace("white-dot", "empty-dot");
+  }
+  // Set the testimonial of the index we are on -1 to be visible
+  testimonials[slideIndex-1].style.display = "block";
+  // Set the dot of the index we are on -1 to be white
+  dots[slideIndex-1].className += "white-dot";
+}
+
+function plusDivs(index) {
+  showDivs(slideIndex += index);
+}
+
+function currentDiv(index) {
+  showDivs(slideIndex = index);
+}
