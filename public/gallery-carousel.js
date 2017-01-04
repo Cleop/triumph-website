b84335e579a78b1fd7bdf770c6ef75ad26d10ada
Cleop <cleo.pearson@hotmail.co.uk>
2017-01-04 14:31:21 +0000

    Writes clear comments explaining carousel function

--- public/gallery-carousel.js
+++ public/gallery-carousel.js
@@ -2,13 +2,20 @@ let myIndex = 0;
 carousel();
 
 function carousel() {
-    let i;
-    var img = document.getElementsByClassName("gallery__img");
-    for (i = 0; i < img.length; i++) {
-       img[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > img.length) {myIndex = 1}
-    img[myIndex-1].style.display = "block";
-    setTimeout(carousel, 3000); // Change image every 3 seconds
+  let i;
+  var image = document.getElementsByClassName("gallery__img");
+// Set all images to not display
+  for (i = 0; i < img.length; i++) {
+    image[i].style.display = "none";
+  }
+// Move up through the index of images by 1 each time the function is called
+  myIndex++;
+// If the index reaches the number of total images, reset it back to the first count again
+  if (myIndex > img.length) {
+    myIndex = 1
+  }
+// Style the image which the index is on to display as block (ie. as visible)
+  image[myIndex-1].style.display = "block";
+// Change image every 3 seconds
+  setTimeout(carousel, 3000);
 }
