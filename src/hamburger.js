f507215fa9c146d22c45375375afdeda654b6d48
Cleop <cleo.pearson@hotmail.co.uk>
2017-01-05 09:47:46 +0000

    Fixes hamburger function so menu displays

--- src/hamburger.js
+++
@@ -1,9 +0,0 @@
-const burgerBtn = document.getElementsByClassName('hamburger')
-
-/* When the user clicks on the button toggle the menu */
-burgerBtn.onclick = toggleMenu();
-
-function toggleMenu() {
-  const menu = document.getElementsByClassName('menu')
-  menu.style.display = (menu.style.display = 'none' ? 'block' : 'none' );
-}
