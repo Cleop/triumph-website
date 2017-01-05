fb45c780673f160b87589ae185f2938a72721bfc
Cleop <cleo.pearson@hotmail.co.uk>
2017-01-05 10:08:51 +0000

    Cross and hamburger button alternates upon click

--- public/hamburger.js
+++ public/hamburger.js
@@ -1,7 +1,11 @@
 document.getElementById('hamburger').addEventListener('click', function(){
   document.getElementById('menu').style.zIndex = '1';
+  document.getElementById('cross').style.display = 'block';
+  document.getElementById('hamburger').style.display = 'none';
 });
 
-// document.getElementById('hamburger').addEventListener('click', function(){
-//   document.getElementById('menu').style.zIndex = '1';
-// });
+document.getElementById('cross').addEventListener('click', function(){
+  document.getElementById('menu').style.zIndex = '-1';
+  document.getElementById('hamburger').style.display = 'block';
+  document.getElementById('cross').style.display = 'none';
+});
